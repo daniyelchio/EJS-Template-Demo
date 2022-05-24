4b6314c3915d3bf9882c6f2e29f71a1c859bf270
Daniyel Cheah <cheahd@mcmaster.ca>
2022-05-24 17:36:48 -0400

    ejs loop

--- index.js
+++ index.js
@@ -9,9 +9,21 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Rocky', 'Steph', 'Joe'
+    ]
+    res.render('cats', { cats})
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', { subreddit });
+})
+
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { rand:num });
+    res.render('random', { num:num });
 })
 
 app.listen(3000, () => {
