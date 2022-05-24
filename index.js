4c7231f93e6f4e7684eedd175243e74826dfec9c
Daniyel Cheah <cheahd@mcmaster.ca>
2022-05-24 18:11:54 -0400

    using data in ejs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -13,17 +14,22 @@ app.get('/cats', (req, res) => {
     const cats = [
         'Blue', 'Rocky', 'Steph', 'Joe'
     ]
-    res.render('cats', { cats})
+    res.render('cats', { cats })
 })
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
-    res.render('subreddit', { subreddit });
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', { ...data });
+    } else {
+        res.render('notfound', { subreddit });
+    }
 })
 
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { num:num });
+    res.render('random', { num: num });
 })
 
 app.listen(3000, () => {
